b3044a6d86feedfddb4362d5df7cd1c0357bfa88
soeun <soeuny6212@gmail.com>
2023-11-26 16:32:32 +0900

    로그인, 회원가입 페이지
    로그인 팝업 효과 - global.js 추가
    회원가입 페이지 - signup.html, signup.css 추가

---
+++ js/global.js
@@ -0,0 +1,11 @@
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.visibility = "visible";
+    popup.style.opacity = "1";
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.visibility = "hidden";
+    popup.style.opacity = "0";
+}
