453aceccb9ef80cdc8386cce1d465713958e6d7b
jo_jo_1206 <jojo1206@protonmail.com>
2023-11-29 10:40:21 +0900

    로그인 회원가입 (테스트 전)

--- models/user.js
+++ models/user.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-const User = mongoose.model('User', UserSchema)
+// 비밀번호 암호화
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
 
-module.exports = User;
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,38 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
+// 회원가입 처리
 router.post('/register', async (req, res) => {
     try {
-        const newUser = new User(req.body);
-        await newUser.save();
-        res.status(201).send('User created');
+        const { username, email, password } = req.body;
+
+        const user = new User({ username, email, password });
+        await user.save();
+        
+        res.status(201).send('User registered successfully');
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send(error.message);
     }
 });
+
+// 로그인 처리
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+  
+        if (!user || !await bcrypt.compare(password, user.password)) {
+            return res.status(401).send('Invalid credentials');
+        }
+  
+        // 세션 또는 토큰 생성 로직 추가
+        res.send('User logged in successfully');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+  
 
 module.exports = router;
