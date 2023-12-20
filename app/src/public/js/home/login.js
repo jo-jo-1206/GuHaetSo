b7a3cb2614ad000f63b033b46427be9155d1fef5
jo_jo_1206 <jojo1206@protonmail.com>
2023-12-21 03:17:47 +0900

    로그인 raw

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -239,5 +239,15 @@ function login() {
     password: password.value,
   };
 
-  console.log(req);
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req),
+  })
+  .then((res) => res.json())
+  .then((res) => {
+    
+  });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,45 @@
 "use_strict";
 
-const home = (req, res) => {
-    res.render("home/home");
+const users = {
+    id: ["zzz11411"],
+    password: ["harrycho0998"],
+}
+
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+    signup: (req, res) => {
+        res.render("home/signup");
+    },
 };
 
-const signup = (req, res) => {
-    res.render("home/signup");
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const password = req.body.password;
+        console.log(id, password);
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.password[idx] === password) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인 실패",
+        })
+    },
 };
 
 module.exports = {
-    home,
-    signup,
+    output,
+    process,
 };
